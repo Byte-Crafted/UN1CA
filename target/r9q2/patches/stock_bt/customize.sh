e5515c165681b128e0ca1fc6818a0a006d7b3fe2
pc <tmp>
2026-08-03 13:31:03 +0530

    target: r9q2: patches: stock_bt: add bt-lib patch
    Since ‎unica/patches/bluetooth/customize.sh will now skip the bt-lib patch for r9q2, I included it here itself

--- target/r9q2/patches/stock_bt/customize.sh
+++ target/r9q2/patches/stock_bt/customize.sh
@@ -1,3 +1,13 @@
 LOG_STEP_IN "- Adding stock btservices apex"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/apex/com.android.bt.apex"
 LOG_STEP_OUT
+
+# ‎unica/patches/bluetooth/customize.sh
+# Disable VaultKeeper support
+# Before: [tbnz w8, #0, #0xbd260]
+# After: [b #0xbd260]
+# Before: [tbnz w8, #0, #0xXXXXXX]
+# After: [b #0xXXXXXX]
+LOG "- Patching \"2897773948050037\" to \"289777392a000014\" in apex_payload/lib64/libbluetooth_jni.so"
+HEX_PATCH "$TMP_DIR/apex_payload/lib64/libbluetooth_jni.so" \
+    "2897773948050037" "289777392a000014" > /dev/null
